544779ff3aa925d7e94d05a1c4d2020db0c21610
ayesha92ahmad <ayesha92ahmad@yahoo.com>
2016-04-27 16:27:08 +0530

    linear search
    incomplete

---
+++ linearSearch.py
@@ -0,0 +1,10 @@
+"""Consider the searching problem:
+Input: A sequence of n numbers A D ha1; a2; : : : ;ani and a value .
+Output: An index i such that  D AOEi or the special value NIL if  does not
+appear in A.
+Write pseudocode for linear search, which scans through the sequence, looking
+for . Using a loop invariant, prove that your algorithm is correct. Make sure that
+your loop invariant fulfills the three necessary properties."""
+
+def linearSearch(A, l):
+    for
